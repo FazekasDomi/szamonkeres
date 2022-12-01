66237fac9969475f741384fd20465c9f4b13b593
FazekasDomi <112930880+FazekasDomi@users.noreply.github.com>
2022-12-01 12:27:17 +0100

    Add files via upload

---
+++ pyton rész.py
@@ -0,0 +1,38 @@
+#1
+'''
+vezeteknev = int(input("szeretnék egy vezetéknevet"))
+keresztnev = int(input("szeretnék egy keresztnevet"))
+nev = vezeteknev+keresztnev
+    print(nev)'''
+#2
+'''
+v = int(input("szeretnék egy számot"))
+k= v-1
+c=v+1
+print(k)
+print(c)
+'''
+#3
+'''
+v1 = int(input("szeretnék egy számot"))
+
+v2 = int(input("szeretnék mégegy számot"))
+
+v3=v1+v2
+v4=v1-v2
+v5=v1*v2
+v6=v1^v2
+
+print(v3,"az összeg")
+print(v4,"a külömbség")
+print(v5,"a szorzat")
+print(v6,"a",v1,"a",v2,"dikon")
+'''
+#4
+a=int(input("szeretnék egy számot"))
+
+b = int(input("szeretnék mégegy számot"))
+c=2*a+3*b
+d=c-2*a-3*b
+print(c)
+print(d)
